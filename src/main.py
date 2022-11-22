2c46830246568b2a4081dc4f378d1d0c518b26b1
pavita1108 <18220014@std.stei.itb.ac.id>
2022-11-22 23:48:22 +0700

    edit fitur gambar

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog , QApplication, QWidget, QStackedWidget, QLabel
 from PyQt5.QtCore import Qt,QSize
-# from PyQt5.QtCore import * 
 from PyQt5.QtGui import QIcon, QPixmap
 import sqlite3
 import cart 
@@ -17,44 +16,19 @@ class Main (QDialog):
         cursor = db.cursor()
         sqlquery = "SELECT * FROM menu"
         result = cursor.execute(sqlquery).fetchall()
-         #metadat: [('M1', 'Bacon, Egg & Cheese Biscuit', 20000, "The McDonald's Bacon, Egg & Cheese Biscuit breakfast sandwich features a warm, buttermilk biscuit brushe
-# d with real butter, thick cut Applewood smoked bacon, a fluffy folded egg, and a slice of melty American cheese."), ('M2', 'Egg McMuffin', 30000, 'Our 
-# Egg McMuffin® breakfast sandwich is an excellent source of protein and oh so delicious. We place a freshly cracked Grade A egg on a toasted English Muf
-# fin topped with real butter and add lean Canadian bacon and melty American cheese.'), ('M3', 'Sausage McMuffin', 25000, "McDonald's Sausage McMuffin® r
-# ecipe features a warm, freshly toasted English muffin, topped with a savory hot sausage patty and a slice of melty American cheese")]
-        # self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setRowCount(len(result))
         completerList = []
-        # for row in range(len(result)):
-        #     completerList.append(str(result[row][1]))
-        #     for column in range(3):
-        #         # print(result[row][column+1])
-        #         item = QtWidgets.QTableWidgetItem(str(result[row][column+1]))
-        #         label = QLabel()
-        #         # pixmap = Qpixmap(column+1,'.jpeg')
-        #         self.tableWidget.setItem(row, column, item)
-        for row_number,row_data in enumerate(result):
-            self.tableWidget.insertRow(row_number)
-            completerList.append(str(result[row_number][1]))
-            for column_number, column_data in enumerate(row_data):
-                it = QtWidgets.QTableWidgetItem()
-                if(column_number == 0):
-                    # print(column_data)
-                    # item = self.getImageLabel(column_data)
-                    imagelabel = QDialog.QLabel(self.centralwidget)
-                    imagelabel.setText("")
-                    imagelabel.setScaledContents(True)
-                    pixmap = QPixmap()
-                    pixmap.loadFromData(column_data)
-                    item = imagelabel.setPixmap(pixmap)
-                    self.tablewidget.setCellWidget(rown_number, column_number, item)
-                    # it.setIcon(QIcon(pixmap.scaled(500, 500)))
-                    # size = QSize(10, 10)
-                    # it.setIconSize(size)
-                else:
-                    item = str(column_data)
-                    it.setText(str(item))
-                    self.tableWidget.setItem(row_number,column_number,it)
-                
+        for row in range(len(result)):
+            completerList.append(str(result[row][1]))
+            for column in range(4):
+                column_data = result[row][column]
+                if (column == 0) :
+                    item = self.getImageLabel(column_data)
+                    self.tableWidget.setCellWidget(row,column,item)
+                else : 
+                    item = QtWidgets.QTableWidgetItem(str(result[row][column]))
+                    self.tableWidget.setItem(row, column, item)
+
         self.searchText.textChanged.connect(self.filtermenu)
         self.allMenuButton.clicked.connect(self.searchText.clear)
         self.tableWidget.clicked.connect(self.getMenu)
@@ -66,11 +40,13 @@ class Main (QDialog):
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)
         self.searchText.setCompleter(self.completer)
 
-    # def getImageLabel(image):
-    #     pixmap = QPixmap()
-    #     pixmap.loadFromData(image,'jpg')
-    #     imageLabel.setPixmap(pixmap)
-    #     return imageLabel
+    def getImageLabel(self,image):
+        imageLabel = QtWidgets.QLabel(self)
+        imageLabel.setScaledContents(True)
+        pixmap = QPixmap()
+        pixmap.loadFromData(image,'jpg')
+        imageLabel.setPixmap(pixmap)
+        return imageLabel
 
     def gtCart(self):
         self.umw = cart.Cart()
@@ -80,7 +56,7 @@ class Main (QDialog):
     
     def filtermenu (self, filter_text):
         for i in range(self.tableWidget.rowCount()):
-            for j in range(self.tableWidget.columnCount()):
+            for j in range (1, self.tableWidget.columnCount()):
                 item = self.tableWidget.item(i, j)
                 match = filter_text.lower() not in item.text().lower()
                 self.tableWidget.setRowHidden(i, match)
--- src/ui_cart.py
+++ src/ui_cart.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\ITB\Sem 5\RPL\coba\cart.ui'
+# Form implementation generated from reading ui file 'd:\ITB\Sem 5\RPL\Tubes\RPL_Kelompok8\src\cart.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
@@ -14,63 +14,279 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Cart(object):
     def setupUi(self, Cart):
         Cart.setObjectName("Cart")
-        Cart.resize(1280, 720)
+        Cart.resize(1279, 720)
+        Cart.setStyleSheet("")
         self.tableWidget = QtWidgets.QTableWidget(Cart)
-        self.tableWidget.setGeometry(QtCore.QRect(20, 120, 1231, 501))
+        self.tableWidget.setGeometry(QtCore.QRect(20, 190, 1231, 431))
+        self.tableWidget.setAutoFillBackground(False)
+        self.tableWidget.setStyleSheet("QTableQidget #tableWidget{\n"
+"    border-style : solid;\n"
+"    border : 0px;\n"
+"    padding : 10px;\n"
+"}")
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setRowCount(0)
         item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(2, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(3, item)
-        self.labelHarga = QtWidgets.QLabel(Cart)
-        self.labelHarga.setGeometry(QtCore.QRect(320, 30, 181, 51))
         font = QtGui.QFont()
         font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.tableWidget.setHorizontalHeaderItem(3, item)
+        self.tableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(250)
+        self.tableWidget.horizontalHeader().setHighlightSections(True)
+        self.tableWidget.horizontalHeader().setSortIndicatorShown(False)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.verticalHeader().setCascadingSectionResizes(True)
+        self.tableWidget.verticalHeader().setDefaultSectionSize(50)
+        self.tableWidget.verticalHeader().setHighlightSections(False)
+        self.tableWidget.verticalHeader().setSortIndicatorShown(False)
+        self.tableWidget.verticalHeader().setStretchLastSection(False)
+        self.labelHarga = QtWidgets.QLabel(Cart)
+        self.labelHarga.setGeometry(QtCore.QRect(320, 110, 181, 51))
+        font = QtGui.QFont()
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.labelHarga.setFont(font)
+        self.labelHarga.setStyleSheet("QLabel#labelHarga{\n"
+"    border-radius : 10px;\n"
+"    border : 1px;\n"
+"    border-style : solid;\n"
+"    font : 20px;\n"
+"    padding : 6px;\n"
+"}")
+        self.labelHarga.setFrameShape(QtWidgets.QFrame.Box)
         self.labelHarga.setText("")
         self.labelHarga.setAlignment(QtCore.Qt.AlignCenter)
         self.labelHarga.setObjectName("labelHarga")
         self.labelNama = QtWidgets.QLabel(Cart)
-        self.labelNama.setGeometry(QtCore.QRect(20, 30, 291, 51))
+        self.labelNama.setGeometry(QtCore.QRect(20, 110, 291, 51))
         font = QtGui.QFont()
-        font.setPointSize(10)
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.labelNama.setFont(font)
+        self.labelNama.setStyleSheet("QLabel#labelNama{\n"
+"    border-radius : 10px;\n"
+"    border : 1px;\n"
+"    border-style : solid;\n"
+"    font : 20px;\n"
+"    padding : 6px;\n"
+"}")
+        self.labelNama.setFrameShape(QtWidgets.QFrame.Box)
         self.labelNama.setText("")
         self.labelNama.setAlignment(QtCore.Qt.AlignCenter)
         self.labelNama.setObjectName("labelNama")
         self.labelTotal = QtWidgets.QLabel(Cart)
-        self.labelTotal.setGeometry(QtCore.QRect(530, 30, 181, 51))
+        self.labelTotal.setGeometry(QtCore.QRect(530, 110, 181, 51))
         font = QtGui.QFont()
-        font.setPointSize(10)
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.labelTotal.setFont(font)
+        self.labelTotal.setStyleSheet("QLabel#labelTotal{\n"
+"    border-radius : 10px;\n"
+"    border : 1px;\n"
+"    border-style : solid;\n"
+"    font : 20px;\n"
+"    padding : 6px;\n"
+"}")
+        self.labelTotal.setFrameShape(QtWidgets.QFrame.Box)
         self.labelTotal.setText("")
         self.labelTotal.setAlignment(QtCore.Qt.AlignCenter)
         self.labelTotal.setObjectName("labelTotal")
         self.kuantitasText = QtWidgets.QLineEdit(Cart)
-        self.kuantitasText.setGeometry(QtCore.QRect(900, 40, 71, 41))
+        self.kuantitasText.setGeometry(QtCore.QRect(900, 120, 71, 41))
         font = QtGui.QFont()
-        font.setPointSize(14)
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.kuantitasText.setFont(font)
+        self.kuantitasText.setStyleSheet("QLineEdit#kuantitasText{\n"
+"    border-radius: 10px;\n"
+"    border:1px;\n"
+"    border-style:solid;\n"
+"    font:14px;\n"
+"    padding:6px;\n"
+"}")
         self.kuantitasText.setText("")
+        self.kuantitasText.setFrame(True)
         self.kuantitasText.setObjectName("kuantitasText")
         self.minButton = QtWidgets.QPushButton(Cart)
-        self.minButton.setGeometry(QtCore.QRect(850, 40, 31, 41))
+        self.minButton.setGeometry(QtCore.QRect(850, 120, 31, 41))
         font = QtGui.QFont()
-        font.setPointSize(15)
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.minButton.setFont(font)
+        self.minButton.setStyleSheet("QPushButton#minButton {\n"
+"    background-color: #56BB86;\n"
+"    border-radius: 10px;\n"
+"    font:30px;\n"
+"    color:#000;\n"
+"    padding: 6px;\n"
+"}\n"
+"QPushButton#minButton:hover {\n"
+"    background-color:rgb(46, 209, 119);\n"
+"    border-style: inset;\n"
+"}\n"
+"\n"
+"QPushButton#minButton:pressed {\n"
+"    background-color:rgb(58, 127, 90);\n"
+"    border-style: inset;\n"
+"}")
         self.minButton.setObjectName("minButton")
         self.plusButton = QtWidgets.QPushButton(Cart)
-        self.plusButton.setGeometry(QtCore.QRect(990, 40, 31, 41))
+        self.plusButton.setGeometry(QtCore.QRect(990, 120, 31, 41))
         font = QtGui.QFont()
-        font.setPointSize(15)
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.plusButton.setFont(font)
+        self.plusButton.setStyleSheet("QPushButton#plusButton {\n"
+"    background-color: #56BB86;\n"
+"    border-radius: 10px;\n"
+"    font:28px;\n"
+"    color:#000;\n"
+"    padding: 6px;\n"
+"}\n"
+"QPushButton#plusButton:hover {\n"
+"    background-color:rgb(46, 209, 119);\n"
+"    border-style: inset;\n"
+"}\n"
+"\n"
+"QPushButton#plusButton:pressed {\n"
+"    background-color:rgb(58, 127, 90);\n"
+"    border-style: inset;\n"
+"}")
         self.plusButton.setObjectName("plusButton")
+        self.resiButton = QtWidgets.QPushButton(Cart)
+        self.resiButton.setGeometry(QtCore.QRect(1100, 640, 141, 41))
+        self.resiButton.setStyleSheet("QPushButton#resiButton {\n"
+"    background-color: rgb(225, 225, 225);\n"
+"    border-radius: 10px;\n"
+"    border:1px;\n"
+"    border-style:solid;\n"
+"    font:16px;\n"
+"    color:#000;\n"
+"    padding: 6px;\n"
+"}\n"
+"QPushButton#resiButton:hover {\n"
+"    background-color:rgb(213, 231, 231);\n"
+"    border-style: inset;\n"
+"}\n"
+"\n"
+"QPushButton#resiButton:pressed {\n"
+"    background-color:rgb(190, 190, 190);\n"
+"    border-style: inset;\n"
+"}")
+        self.resiButton.setObjectName("resiButton")
+        self.cartHeader = QtWidgets.QFrame(Cart)
+        self.cartHeader.setGeometry(QtCore.QRect(0, 0, 1281, 71))
+        self.cartHeader.setStyleSheet("QFrame#cartHeader{\n"
+"    background-color : rgb(86, 187, 134)\n"
+"}")
+        self.cartHeader.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.cartHeader.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.cartHeader.setObjectName("cartHeader")
+        self.cartLabel = QtWidgets.QLabel(self.cartHeader)
+        self.cartLabel.setGeometry(QtCore.QRect(550, 10, 181, 51))
+        font = QtGui.QFont()
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.cartLabel.setFont(font)
+        self.cartLabel.setStyleSheet("QLabel#cartLabel{\n"
+"    font:32px;\n"
+"    font-weight: bold;\n"
+"    color:#fff;\n"
+"}")
+        self.cartLabel.setObjectName("cartLabel")
+        self.back_2 = QtWidgets.QPushButton(self.cartHeader)
+        self.back_2.setGeometry(QtCore.QRect(30, 10, 131, 41))
+        font = QtGui.QFont()
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.back_2.setFont(font)
+        self.back_2.setStyleSheet("QPushButton#back_2 {\n"
+"    background-color: rgb(86, 187, 134);\n"
+"    border-radius: 10px;\n"
+"    font:28px;\n"
+"    font-weight: bold;\n"
+"    color:#fff;\n"
+"    padding: 6px;\n"
+"}\n"
+"\n"
+"\n"
+"QPushButton#back_2:pressed {\n"
+"    color:#ddd;\n"
+"    border-style: inset;\n"
+"}")
+        self.back_2.setObjectName("back_2")
+        self.label_4 = QtWidgets.QLabel(Cart)
+        self.label_4.setGeometry(QtCore.QRect(20, 90, 151, 21))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(Cart)
+        self.label_5.setGeometry(QtCore.QRect(320, 90, 181, 20))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.label_2 = QtWidgets.QLabel(Cart)
+        self.label_2.setGeometry(QtCore.QRect(530, 90, 181, 20))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.cartHeader.raise_()
+        self.tableWidget.raise_()
+        self.labelHarga.raise_()
+        self.labelNama.raise_()
+        self.labelTotal.raise_()
+        self.kuantitasText.raise_()
+        self.minButton.raise_()
+        self.plusButton.raise_()
+        self.resiButton.raise_()
+        self.label_4.raise_()
+        self.label_5.raise_()
+        self.label_2.raise_()
 
         self.retranslateUi(Cart)
         QtCore.QMetaObject.connectSlotsByName(Cart)
@@ -88,3 +304,9 @@ class Ui_Cart(object):
         item.setText(_translate("Cart", "Total"))
         self.minButton.setText(_translate("Cart", "-"))
         self.plusButton.setText(_translate("Cart", "+"))
+        self.resiButton.setText(_translate("Cart", "Cetak Resi"))
+        self.cartLabel.setText(_translate("Cart", "Keranjang"))
+        self.back_2.setText(_translate("Cart", "< Back"))
+        self.label_4.setText(_translate("Cart", "Nama Makanan"))
+        self.label_5.setText(_translate("Cart", "Harga (Rp)"))
+        self.label_2.setText(_translate("Cart", "Total (Rp)"))
